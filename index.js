405a618df6882493d5b1a06368bd5281573d09f8
gabrieldasilvadev <gabrieldasilvaprivado1@gmail.com>
2021-12-03 16:29:02 -0300

    Reconstrucao das models

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
-const port = 3000;
+const port = 8080;
 
+// models
 const conn = require('./db/conn');
 const Empreendedor = require('./models/Empreendedor');
-const empreendedorRoutes = require('./routes/empreendedorRoutes');
+const Usuario = require('./models/Usuario');
 
+// routes
+
+
+// template engine
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
+// receber resposta do body
 app.use(
   express.urlencoded({
     extended: true
@@ -17,9 +23,14 @@ app.use(
 );
 
 app.use(express.json());
+// public path
 app.use(express.static('public'));
 
-app.use('/empreendedor', empreendedorRoutes);
+// app.use Routes
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
 
 conn.sync()
     .then(() => {
--- routes/empreendedorRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const EmpreendedorController = require('../controllers/EmpreendedorController');
-
-router.get('/add', EmpreendedorController.createEmpreendedor);
-router.get('/', EmpreendedorController.showEmpreendedor);
-
-module.exports = router;
