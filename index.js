93324137341ccac0a0cdc49281551543725b04d3
gabrieldasilvadev <gabrieldasilvaprivado1@gmail.com>
2021-12-11 18:40:09 -0300

    adiciona imabens landing page e alteracao no css

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const port = 3000;
-
 const app = express();
 
 // models
 const conn = require('./db/conn');
-// const Empreendedor = require('./models/Empreendedor');
+
+// eslint-disable-next-line no-unused-vars
 const Usuario = require('./models/Usuario');
+
+
+app.engine('handlebars', exphbs.engine(
+  { defaultLayout: 'main' },
+  {partialsDir: 'views/partials'},
+  ));
+app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.static('public'));
 
 // routes
 const cadastroUsuarioRoutes = require('./routes/cadastroUsuarioRoutes');
-
-// template engine
-app.engine('handlebars', exphbs.engine());
-app.set('view engine', 'handlebars');
 
 // receber resposta do body
 app.use(
@@ -23,16 +28,12 @@ app.use(
   })
 );
 
-app.use(express.json());
-// public path
-app.use(express.static('public'));
-
-app.use('/usuario', cadastroUsuarioRoutes);
-
 // app.use Routes
 app.get('/', (req, res) => {
-  res.render('layouts/main');
+  res.render('home');
 });
+
+app.use('/usuario', cadastroUsuarioRoutes);
 
 conn
   .sync({ alter: true })
