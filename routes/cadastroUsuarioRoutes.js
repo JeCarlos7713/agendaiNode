9470bf31794e745671e1cd3269f5c6a9daf48af2
gabrieldasilvadev <gabrieldasilvaprivado1@gmail.com>
2021-12-12 16:11:14 -0300

    Adiciona rotas para usuario

--- routes/cadastroUsuarioRoutes.js
+++ routes/cadastroUsuarioRoutes.js
@@ -1,9 +1,17 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 
 const UsuarioController = require('../controllers/UsuarioController');
 
-router.get('/cadastro', UsuarioController.cadastraUsuario);
-router.post('/cadastro', UsuarioController.usuarioCadastrado);
+router.post('/cadastro', UsuarioController.cadastro);
+router.post('/login', UsuarioController.login);
+router.get('/checkuser', UsuarioController.checkUser);
+
+// router.post('/register', UsuarioController.register);
+
+// const UsuarioController = require('../controllers/UsuarioController');
+
+// router.get('/cadastro', UsuarioController.pageCadastro);
+// router.get('/cadastro', UsuarioController.cadastraUsuario);
+// router.post('/cadastro', UsuarioController.cadastraUsuarioPost);
 
 module.exports = router;
